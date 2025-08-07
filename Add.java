d0bd478d13b583b8908fb284347d8e9625f601fd
ayushsahu01 <ayushsahu29042005@gmail.com>
2025-08-07 15:11:19 +0530

    [Ayush] Add . Adding new line in 8

--- Add.java
+++ Add.java
@@ -5,13 +5,12 @@ public class Add {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c=50;
+        System.out.println("Adding by me Hey Buddy");
         System.out.println("Addition of " + a + " and " + b + " is : " + add(a, b));
         System.out.println("Adding some new Features in Addition Function");
         System.out.println("Changes by me Aush in line no. 9");
-        int c = 50;
         sc.close();
         System.out.println("Don't add any features everyone");
-        int c=50;
     }
     static int add(int a, int b) {
         return a + b;
